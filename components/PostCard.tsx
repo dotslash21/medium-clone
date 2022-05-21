9ed79ae7c44424fc6328325f69d16491ca8796e7
Arunangshu Biswas <arunangshubsws@gmail.com>
2022-05-21 12:25:58 +0530

    Fetch posts from sanity and show as card on home

---
+++ components/PostCard.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import React from 'react'
+import { urlFor } from '../sanity'
+import { Post } from '../typings'
+
+interface Props {
+    post: Post
+}
+
+function PostCard({ post }: Props) {
+  return (
+    <Link key={post._id} href={`/post/${post.slug.current}`}>
+      <div className="group cursor-pointer overflow-hidden rounded-lg border">
+        <img
+          className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+          src={urlFor(post.mainImage).url()!}
+          alt={post.title}
+        />
+        <div className="flex justify-between bg-white p-5">
+          <div>
+            <p className="text-lg font-bold">{post.title}</p>
+            <p className="text-xs">
+              {post.description} by {post.author.name}
+            </p>
+          </div>
+
+          <img
+            className="h-12 w-12 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt={post.author.name}
+          />
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default PostCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import PostCard from '../components/PostCard'
+import { sanityClient } from '../sanity'
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: Post[]
+}
+
+export const getServerSideProps = async () => {
+  const query = `
+  *[_type == "post"]{
+    _id,
+    title,
+    author -> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }
+  `
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+const Home = ({ posts }: Props) => {
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -16,6 +46,11 @@ const Home: NextPage = () => {
       <Banner />
 
       {/* Posts */}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <PostCard post={post} />
+        ))}
+      </div>
     </div>
   )
 }
